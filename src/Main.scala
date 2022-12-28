1a95dd7a6d0b2304d18e7eb854351fb8a2631cc5
Treese <treesega@buffalo.edu>
2022-12-27 21:57:05 -0500

    rev 2 working average, but needs a place to input exercise name

--- src/Main.scala
+++ src/Main.scala
@@ -10,6 +10,7 @@ import scala.collection.convert.ImplicitConversions.`collection asJava`
 import java.time.{DayOfWeek, LocalDate}
 import scala.jdk.Accumulator
 import java.time.temporal.TemporalAdjusters
+import scala.collection.immutable.ListMap
 //import play.api.data._
 //import play.api.data.Forms._
 
@@ -20,8 +21,21 @@ object Main {
   val inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
   val dateFormat = new SimpleDateFormat("yyyy-MM-dd")
   val yearFormat = new SimpleDateFormat("yyyy")
+  val templateWeekMap:Map[Int,Double] = Map (
+    0->0, 1->0, 2->0, 3->0, 4->0,
+    5->0, 6->0, 7->0, 8->0, 9->0,
+    10->0, 11->0, 12->0, 13->0, 14->0,
+    15->0, 16->0, 17->0, 18->0, 19->0,
+    20->0, 21->0, 22->0, 23->0, 24->0,
+    25->0, 26->0, 27->0, 28->0, 29->0,
+    30->0, 31->0, 32->0, 33->0, 34->0,
+    35->0, 36->0, 37->0, 38->0, 39->0,
+    40->0, 41->0, 42->0, 43->0, 44->0,
+    45->0, 46->0, 47->0, 48->0, 49->0,
+    50->0, 51->0, 52->0, 53->0
+  )
 
-
+  /*
   //DONE
   //Will return a map with [Week(INT) -> Dates within that week]
   def dateToWeekMapper(workoutLogFilename: String, selectedYear: Int): Map[Int,ListBuffer[String]] = {
@@ -56,24 +70,6 @@ object Main {
     return weekToDateTracker
   }
 
-
-  //DONE
-  //Calculates what week of the year a certain date is
-  def weekCalculator(inputDate:String): Int={
-    val date = dateFormat.parse(inputDate)
-    //Creates calendar and sets first day of week to Monday
-    val cal = Calendar.getInstance
-    cal.setTime(date)
-    cal.setFirstDayOfWeek(Calendar.MONDAY)
-
-    val weekNumber = cal.get(Calendar.WEEK_OF_YEAR)
-    return weekNumber
-  }
-
-
-  //VOLUME CALCULATION FUNCTIONS START HERE
-
-
   //Computes average volume per rep
   def volumeAdder(workoutLogFilename: String, weekToDateMap: Map[Int,ListBuffer[String]], selectedExercise: String): Map[Int,Double] = {
     val workoutFile: BufferedSource = Source.fromFile(workoutLogFilename)
@@ -103,23 +99,130 @@ object Main {
     }
     return weekToVolumeMap
   }
+   */
 
-  def repAdder: Double = {
-    0.0
+
+
+  //UTILITY FUNCTIONS
+  def weekCalculator(inputDate:String): Int={
+    val date = dateFormat.parse(inputDate)
+    //Creates calendar and sets first day of week to Monday
+    val cal = Calendar.getInstance
+    cal.setTime(date)
+    cal.setFirstDayOfWeek(Calendar.MONDAY)
+
+    val weekNumber = cal.get(Calendar.WEEK_OF_YEAR)
+    return weekNumber
   }
 
+  def filenameToListOfLines(filename: String): List[String] = {
+    val file = Source.fromFile(filename)
+    val lines: List[String] = file.getLines().toList.drop(1)
+    file.close()
+    return lines
+  }
+
+  //MAPPING FUNCTIONS
+  def weekToVolumeHelper(filename: String, year: Int): ListMap[Int,Double] ={
+    val data = filenameToListOfLines(filename)
+    val output = weekToVolumeMapper(data, year, templateWeekMap, 0)
+    return output
+  }
+
+  def weekToVolumeMapper(data:List[String], year: Int, accumulator:Map[Int,Double], tracker:Int): ListMap[Int,Double] ={
+    if (tracker == data.length){
+      //println(tracker)
+      val output = ListMap(accumulator.toSeq.sortBy(_._1):_*)
+      return output
+    }
+    else {
+      //Setting up variables
+      val lineList = data(tracker).split(",")
+      //println(lineList(index))
+      val currentDate: String = dateFormat.format(inputFormat.parse(lineList(0)))
+      //println("Here's the current date " + currentDate)
+      val currentWeek: Int = weekCalculator(currentDate)
+      //println("Here's the current week " + currentWeek)
+      val currentYear: Int = yearFormat.format(inputFormat.parse(lineList(0))).toInt
+      //println("Here's the current year " + currentYear)
+      val currentExercise: String = lineList(2)
+      //println("Here's the current exercise " + currentExercise)
+      val setTotalVolume: Double = lineList(3).toInt * lineList(5).toDouble
+      //println("Here's the current volume " + setTotalVolume)
+
+      //Performing the math and adding to the accumulator
+      if (currentYear == year) {
+        weekToVolumeMapper(data, year, accumulator + (currentWeek -> (accumulator(currentWeek) + setTotalVolume)), tracker + 1)
+      }
+      else{
+        weekToVolumeMapper(data, year, accumulator, tracker + 1)
+      }
+    }
+  }
+
+  def weekToRepCountHelper(filename: String, year: Int): ListMap[Int,Double] ={
+    val data = filenameToListOfLines(filename)
+    val output = weekToRepCountMapper(data, year, templateWeekMap, 0)
+    return output
+  }
+
+  def weekToRepCountMapper(data:List[String], year: Int, accumulator:Map[Int,Double], tracker:Int): ListMap[Int,Double] ={
+    if (tracker == data.length){
+      //println(tracker)
+      val output = ListMap(accumulator.toSeq.sortBy(_._1):_*)
+      return output
+    }
+    else {
+      //Setting up variables
+      val lineList = data(tracker).split(",")
+      //println(lineList(index))
+      val currentDate: String = dateFormat.format(inputFormat.parse(lineList(0)))
+      //println("Here's the current date " + currentDate)
+      val currentWeek: Int = weekCalculator(currentDate)
+      //println("Here's the current week " + currentWeek)
+      val currentYear: Int = yearFormat.format(inputFormat.parse(lineList(0))).toInt
+      //println("Here's the current year " + currentYear)
+      val currentExercise: String = lineList(2)
+      //println("Here's the current exercise " + currentExercise)
+      val setRepTotal: Double = lineList(3).toDouble
+      //println("Here's the current volume " + setTotalVolume)
+
+      //Performing the math and adding to the accumulator
+      if (currentYear == year) {
+        weekToRepCountMapper(data, year: Int, accumulator + (currentWeek -> (accumulator(currentWeek) + setRepTotal)), tracker + 1)
+      }
+      else{
+        weekToRepCountMapper(data, year: Int, accumulator, tracker + 1)
+      }
+    }
+  }
+
+  //AVERAGING FUNCTIONS
+  def weightPerRepAveragerHelper(filename: String, year: Int): ListMap[Int, Double] ={
+    val output = weightPerRepAverager(filename, year, templateWeekMap, 0)
+    return output
+  }
+
+  def weightPerRepAverager(filename: String, year: Int, accumulator:Map[Int,Double], tracker: Int): ListMap[Int, Double] ={
+    val weekToVolumeMap:ListMap[Int,Double] = weekToVolumeHelper(filename, year)
+    val weekToRepMap:ListMap[Int,Double] = weekToRepCountHelper(filename, year)
+    if (tracker == 54){
+      val output = ListMap(accumulator.toSeq.sortBy(_._1):_*)
+      return output
+    }
+    else{
+      val weightPerRep: Double = weekToVolumeMap(tracker) / weekToRepMap(tracker)
+      weightPerRepAverager(filename, year, accumulator + (tracker -> weightPerRep), tracker + 1)
+    }
+  }
 
   def main(args: Array[String]): Unit = {
-    //dateToWeekMapper("data/test_data.csv", 0)
-    val testMap = dateToWeekMapper("data/test_data.csv", 2019)
-    for ((k,v) <- testMap){
-      //println(k)
-      //println(v)
-    }
-    val testResult = volumeAdder("data/test_data.csv", testMap, "Bench Press")
-    for ((k,v) <- testResult){
-      //println(k)
-      //println(v)
+    val testRepTotals = weekToRepCountHelper("data/test_data.csv", 2019)
+    val testVolumeTotals = weekToVolumeHelper("data/test_data.csv", 2019)
+    val testAverages = weightPerRepAveragerHelper("data/test_data.csv", 2019)
+    for ((k,v) <- testAverages){
+      println(k)
+      println(v)
     }
   }
 
